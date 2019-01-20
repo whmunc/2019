d2ae9ff1ca64f3df9fae7d27fd06942c463347ef
dingyx99 <dingyuxiao99@nwafu.edu.cn>
2019-01-20 23:39:05 +0800

    Update sworker.js

--- sworker.js
+++ sworker.js
@@ -5,11 +5,7 @@ self.addEventListener('install', function(event) {
         caches.open('static-cache').then(function (cache) {
             return cache.addAll([
                 '/',
-                '/show.html',
-                '/manifest/favicon/favicon-16x16.png',
-                '/manifest/favicon/favicon-96x96.png',
-                '/manifest/android-chrome-192x192.png',
-                '/manifest/favicon/favicon-194x194.png',
+                'show.html',
                 '/css/editor.css',
                 '/css/narrow-jumbotron.css',
                 '/images/19WHMUNC-1@0,5x.png',
